5a1fd6c90f0bfa86a8c6cf6baf4fe739c16a3597
parthvyas18896 <pvyas1@confederationcollege.ca>
2021-02-01 11:11:01 -0500

    Step 3 Project 1

--- project1/js/chapter09-project01.js
+++ project1/js/chapter09-project01.js
@@ -1,10 +1,26 @@
 
 /* add code here  */
-window.addEventListener( 'load', function(){
+window.addEventListener('load', function(){
 var hilightableInputs = document.querySelectorAll(".hilightable");
 for ( i=0 ; i<hilightableInputs.length ; i++) {
     var toggle = (event) => { event.target.classList.toggle('highlight')}
     hilightableInputs[i].addEventListener('focus', toggle)
     hilightableInputs[i].addEventListener('blur', toggle)
+}
+
+var form = document.getElementById("mainForm");
+form.addEventListener("submit", submissionCheck);
+function submissionCheck(event)
+{
+    var requiredInputs = document.querySelectorAll(".required");
+    for (i=0; i<requiredInputs.length;i++)
+    {
+        if (requiredInputs[i].value=="")
+        {
+            event.preventDefault();
+            requiredInputs[i].classList.add("error");
+        }
+        
+    }
 }
 })
