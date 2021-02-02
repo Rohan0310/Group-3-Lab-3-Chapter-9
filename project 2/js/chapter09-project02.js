79b1380987a5d482dafe954e3b2c1a4cdbc1758b
Neel Patel <npatel16@confederationcollege.ca>
2021-02-01 21:03:18 -0500

    Lab 3 Project 2 Step 3 done

--- project 2/js/chapter09-project02.js
+++ project 2/js/chapter09-project02.js
@@ -1,4 +1,25 @@
 /* add code here */
-window.addEventListener('load', function() {
- 
-});
+window.addEventListener('load', function() 
+ {
+	var thumbs = document.getElementById("thumbnails");
+       var element = document.querySelector('figcaption');
+       
+       thumbs.addEventListener("click", function (a)
+      {
+
+       if (a.target.nodeName.toLowerCase() == 'img') 
+       {
+            var clickedImageSource = a.target.src;
+
+	        var newSrc = clickedImageSource.replace("small", "medium");           
+
+
+     	    var featuredImage = document.querySelector("#featured img");
+       		featuredImage.src = newSrc;
+       		featuredImage.title = a.target.title;
+       		element.innerHTML = a.target.title;
+ 	
+       		
+        }
+       }); 
+ });
